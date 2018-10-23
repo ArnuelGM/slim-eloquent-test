de6d4b4206c9b1ca67bb27f863ef0658e3d52138
= <arnuel.gutierrez@innovasoftcol.com>
2018-10-23 16:57:37 -0500

    servir archivos estaticos

--- src/Controllers/PacientesController.php
+++ src/Controllers/PacientesController.php
@@ -1,18 +1,34 @@
 <?php 
 
-
+	
 	namespace App\Controllers;
 
+	use Psr\Container\ContainerInterface as Container;
 	use App\Models\Pacientes;
 
 	class PacientesController extends Controller {
 
+		protected $container;
+
+		// constructor receives container instance
+		public function __construct(Container $container) {
+			$this->container = $container;
+		}
 
 		public function index($req, $res)
 		{
 
 			return $res->withJson( [ "data" => Pacientes::all() ], 200 );
 
+		}
+
+		public function show($req, $res, $args)
+		{
+			$data = Pacientes::where( 'autoid', '=', $args['id'] )->get()->first();
+			if( !empty( $args['field'] ) ){
+				$data = $data->{$args['field']};
+			}
+			return $res->withJson( [ "data" => $data ], 200 );
 		}
 
 
--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,25 @@
 <?php
 
-	$app->get('/', 'PacientesController:index');
+	use App\Controllers\PacientesController;
+
+	$app->group('/pacientes', function(){
+
+		$this->get('', PacientesController::class . ':index');
+		$this->get('/{id}[/{field}]', PacientesController::class . ':show');
+
+	});
+
+	$app->get('/foto', function($req, $res){
+
+		$image = @file_get_contents( __DIR__ . "/../public/foto.jpg");
+		
+		if($image === FALSE) 
+		{
+			$handler = $this->notFoundHandler;
+			return $handler($req, $res);
+		}
+
+		$res->write($image);
+		return $res->withHeader('Content-Type', FILEINFO_MIME_TYPE);
+
+	});
